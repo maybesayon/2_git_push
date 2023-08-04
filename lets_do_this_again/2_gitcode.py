abd6e7550bcf9ce3ad2d452879948f34cce8f190
maybesayon <sayoncoldplay@gmail.com>
2023-08-04 08:22:06 +0545

    this is it

---
+++ lets_do_this_again/2_gitcode.py
@@ -0,0 +1,7 @@
+message = "Hello World"
+result = message.find("l")
+print(result) #2
+result = message.find("Wor")
+print(result) #6
+result = message.find("wor")
+print(result) #-1
---
+++ lets_do_this_again/gitcode.py
@@ -0,0 +1,7 @@
+message = "Hello World"
+print(message * 3) # Hello WorldHello WorldHello World]
+#Membership Operation
+message = "Hello World"
+print("H" in message) #True
+print("W" not in message) #False
+print("k" in message) #False
